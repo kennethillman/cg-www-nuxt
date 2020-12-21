8a1d9da7411cb5543af0bb55202f5482e68c1585
Kenneth Illman <kenneth.illman@gmail.com>
2020-12-21 12:54:55 +0100

    removed svg

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,7 @@
 
     <div class="cg-content" style="height: 200vh; position:relative">  {{document.data.header[0].text}} <br>
 
-           <svg id="demo-svg" width="600" height="600">
-  <polygon stroke="none" fill="red" points="87 472,87 91,267 80,501 78,489 304,500 450,293 457"></polygon>
-</svg>
+
       <TextSplit text="Expertise" split="5" />
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <TextSplit text="Used by millions" split="11" position="right" />
