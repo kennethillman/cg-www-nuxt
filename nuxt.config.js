52b13656ef0fb8985a62d671c6a5989ab63d7809
Kenneth Illman <kenneth.illman@gmail.com>
2021-02-12 14:52:44 +0100

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -116,6 +116,9 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
 
+    // Makes an external css file
+    extractCSS:  true,
+
     extend(config, ctx) {
       config.node = {
         fs: 'empty',
