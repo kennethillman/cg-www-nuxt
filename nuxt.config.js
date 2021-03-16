452b7d106d89f2890031c21bbade42577848c9cb
Kenneth Illman <kenneth.illman@gmail.com>
2021-03-16 09:43:58 +0100

    Added favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,9 @@ export default {
       { hid: 'robots', name: 'robots', content: 'index, follow '}
     ],
     link: [
+      { rel: 'apple-touch-icon', size: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
