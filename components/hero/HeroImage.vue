d978cfe72d7ffe7a623bdfdb59bf2802e642a06c
Kenneth Illman <kenneth.illman@gmail.com>
2021-03-30 10:14:53 +0200

    portrait / landscape image

--- components/hero/HeroImage.vue
+++ components/hero/HeroImage.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="cg-hero-image"
-      :style="{ backgroundImage: `url(${hero.background_image.url})` }">
-  </div>
+
+<div>
+  <template v-if="screen.ratio === 'landscape'">
+      <div
+        v-if="hero.background_image"
+        class="cg-hero-image"
+        :style="{ backgroundImage: `url(${hero.background_image.url})` }">
+      </div>
+  </template>
+    <template v-if="screen.ratio === 'portrait'">
+      <div
+        v-if="hero.background_image_vertical"
+        class="cg-hero-image"
+        :style="{ backgroundImage: `url(${hero.background_image_vertical.url})` }">
+      </div>
+  </template>
+
+  <h1>{{screen.ratio}}</h1>
+
+</div>
+
 
 </template>
 
 <script type="text/javascript">
 
   export default {
-      props: ['hero']
-    };
+      props: ['hero'],
+      data() {
+        return {
+          screen: {
+            ratio: 0
+          }
+        }
+      },
+      mounted() {
+
+        this.screen.ratio = window.innerWidth >= window.innerHeight ? 'landscape' : 'portrait'
+
+        window.addEventListener('resize', () => {
+          this.screen.ratio = window.innerWidth >= window.innerHeight ? 'landscape' : 'portrait'
+        })
+
+      },
+      beforeDestroy() {
+        window.removeEventListener('resize');
+      },
+  };
 
 </script>
 
